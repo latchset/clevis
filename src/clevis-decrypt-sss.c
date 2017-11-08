7f335a05447213c4b5a8c41680d592dba68a0b20
Nathaniel McCallum <npmccallum@redhat.com>
2017-11-07 19:41:35 -0500

    Add OpenSSL linking exception
    This was always intended by us, but we failed to include this as a mere
    oversight.

--- src/clevis-decrypt-sss.c
+++ src/clevis-decrypt-sss.c
@@ -15,6 +15,24 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GPLv3 section 7:
+ *
+ * In the following paragraph, "GPL" means the GNU General Public
+ * License, version 3 or any later version, and "Non-GPL Code" means
+ * code that is governed neither by the GPL nor a license
+ * compatible with the GPL.
+ *
+ * You may link the code of this Program with Non-GPL Code and convey
+ * linked combinations including the two, provided that such Non-GPL
+ * Code only links to the code of this Program through those well
+ * defined interfaces identified in the file named EXCEPTION found in
+ * the source code files (the "Approved Interfaces"). The files of
+ * Non-GPL Code may instantiate templates or use macros or inline
+ * functions from the Approved Interfaces without causing the resulting
+ * work to be covered by the GPL. Only the copyright holders of this
+ * Program may make changes or additions to the list of Approved
+ * Interfaces.
  */
 
 #define _GNU_SOURCE
